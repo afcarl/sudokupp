c86c85ae0c9ab3b8c297e2d2b5b8453b0e658924
Daniel Connelly <dhconnelly@gmail.com>
2013-02-12 21:31:34 +0100

    stub solve functions

--- solve.cc
+++ solve.cc
@@ -1,6 +1,25 @@
 #include "solve.h"
 
+bool solved(const Puzzle& puzzle) {
+  return true;
+}
+
+bool eliminate(Puzzle& puzzle) {
+  return false;
+}
+
+Square guess(const Puzzle& puzzle) {
+  return Square();
+}
+
 Puzzle solve_puzzle(const Puzzle& puzzle) {
+  // While puzzle is not solved:
+  // 1. Run elimination until there are no changes.
+  //    - If a contradiction is found, throw an exception.
+  // 2. Choose a square and make a guess.
+  //    a. Attempt to solve with that guess.
+  //    b. If solving fails, eliminate that guess.
+  //    c. Otherwise, return the puzzle.
   return puzzle;
 }
 
--- solve.h
+++ solve.h
@@ -7,4 +7,15 @@
 // Throws an invalid_argument exception if the puzzle is not solvable.
 Puzzle solve_puzzle(const Puzzle& puzzle);
 
+// Determines whether a puzzle is solved.
+bool solved(const Puzzle& puzzle);
+
+// Run elimination on a puzzle.
+// Returns true if the puzzle was modified and false otherwise.
+// Throws an invalid_argument exception if a contradiction is found.
+bool eliminate(Puzzle& puzzle);
+
+// Choose a square and guess its value.
+Square guess(const Puzzle& puzzle);
+
 #endif
