4ac24802be45051a8b6a3a3843dac8e8cd324b31
Daniel Connelly <dhconnelly@gmail.com>
2013-02-15 19:48:20 +0100

    stringifiers

--- puzzle.cc
+++ puzzle.cc
@@ -24,28 +24,34 @@ Puzzle make_puzzle() {
 }
 
 std::istream& read_puzzle(Puzzle& puzzle, std::istream& stream) {
+  std::string puzzle_str;
   for (int i = 0; i < ROWS; i++) {
     std::string row;
     if (!(stream >> row))
       return stream;
-    for (int j = 0; j < COLS; j++) {
-      puzzle[i][j] = Square();
-      puzzle[i][j].row = i;
-      puzzle[i][j].col = j;
-      puzzle[i][j].val = row[j] - '0';
-    }
+    puzzle_str += row;
   }
+  read_puzzle(puzzle, puzzle_str);
   return stream;
 }
 
+void read_puzzle(Puzzle& puzzle, const std::string& puzzle_str) {
+  for (int i = 0; i < puzzle_str.size(); i++)
+    puzzle[i / COLS][i % COLS].val = puzzle_str[i] - '0';
+}
+
 std::ostream& write_puzzle(const Puzzle& puzzle, std::ostream& stream) {
-  for (Puzzle::const_iterator row = puzzle.begin(); row != puzzle.end(); row++) {
-    for (Row::const_iterator sq = row->begin(); sq != row->end(); sq++) {
-      stream << sq->val;
-    }
-    stream << std::endl;
+  return stream << write_puzzle(puzzle);
+}
+
+std::string write_puzzle(const Puzzle& puzzle) {
+  std::string puzzle_str;
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++)
+      puzzle_str.push_back(puzzle[i][j].val + '0');
+    puzzle_str.push_back('\n');
   }
-  return stream;
+  return puzzle_str;
 }
 
 Puzzle copy_puzzle(const Puzzle& puzzle) {
--- puzzle.h
+++ puzzle.h
@@ -7,6 +7,8 @@
 const int EMPTY = 0;
 const int ROWS = 9;
 const int COLS = 9;
+const int GROUP_ROWS = 3;
+const int GROUP_COLS = 3;
 
 struct Square {
   int row;
@@ -24,8 +26,14 @@ Puzzle make_puzzle();
 // Read a puzzle from an input stream.
 std::istream& read_puzzle(Puzzle& puzzle, std::istream& stream);
 
+// Read a puzzle from a string.
+void read_puzzle(Puzzle& puzzle, const std::string& puzzle_str);
+
 // Write a puzzle to an input stream.
 std::ostream& write_puzzle(const Puzzle& puzzle, std::ostream& stream);
+
+// Write a puzzle to a string.
+std::string write_puzzle(const Puzzle& puzzle);
 
 // Make a copy of a puzzle.
 Puzzle copy_puzzle(const Puzzle& puzzle);
